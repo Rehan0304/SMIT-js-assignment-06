aefadbdf4c8a89cf0a51564a7ddf8c47222801ff
Muhammad Rehan <qamarzaman.comkhan786@gmail.com>
2023-10-27 23:10:12 +0500

    SMIT assignment JS 07

---
+++ app.js
@@ -0,0 +1,30 @@
+//! CHAPTER 38 to 42
+
+//todo Q 01
+
+// function power(a,b) {
+//     let result=a**b;
+//     console.log(result)
+// }
+// power(4,4);
+
+//todo Q 02
+
+// let enterYear=Number(prompt(`for checking leap year please enter year in numbers `))
+// function leapYear(year) {
+//     let requireYear=year%4;
+//     if (requireYear===0) {
+//         console.log(`${year} leap year`);
+//     }else{
+//         console.log(`it's not leap year`)
+//     }
+// }
+// leapYear(enterYear);
+
+//todo Q 03
+
+
+
+
+
+
